454f38072244aa7e11c948f16313cf3b34763e2a
Juan Aguilar <Juan.aguilartorres@ucr.ac.cr>
2025-03-16 22:36:32 -0600

    📥 Importing project, added simple music theory

---
+++ config/musicTheory.py
@@ -0,0 +1,10 @@
+# ---------------------------
+# Scale and Key Settings
+# ---------------------------
+
+# Scales
+major_pentatonic = [0, 2, 4, 7, 9]
+minor_pentatonic = [0, 3, 5, 7, 10]
+
+# Keys
+natural_notes = {"C": 0, "D": 2, "E": 4, "F": 5, "G": 7, "A": 9, "B": 11}
