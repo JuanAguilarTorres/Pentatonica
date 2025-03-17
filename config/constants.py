9f9d9bb0d9470aedf4da58a9bb6c19f86269bb78
Juan Aguilar <Juan.aguilartorres@ucr.ac.cr>
2025-03-16 22:36:21 -0600

    📥 Importing project, added constants

---
+++ config/constants.py
@@ -0,0 +1,18 @@
+# Useful configurable constants
+grid_rows = 16
+grid_cols = 32
+max_dropdown_rows = 5 
+
+# Other configurable constants
+cell_size = 40
+control_height = 200  
+note_length_ms = 100
+matrix_control_height = 40
+instrument_item_height = 30
+column_width = 150
+
+# Calculation constants
+grid_width = cell_size * grid_cols
+grid_height = cell_size * grid_rows
+height = grid_height + control_height
+width = grid_width
